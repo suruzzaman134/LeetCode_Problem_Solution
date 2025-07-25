0cb09c525fe1ea10a1f85147c5e5d4038ace828c
suruzzaman <mdsuruzzaman134@gmail.com>
2025-07-25 16:30:19 +0600

    commit

--- 42. Trapping Rain Water.cpp
+++ 42. Trapping Rain Water.cpp
@@ -4,21 +4,19 @@ public:
         int n = height.size();
         int l = 0, r = n - 1;
         int lmax = 0, rmax = 0;
-        int ans = 0;
+        int result = 0;
         while(l < r){
             lmax = max(lmax, height[l]);
             rmax = max(rmax, height[r]);
 
             if(lmax < rmax){
-                ans += lmax - height[l];
+                result += lmax - height[l];
                 l++;
             }else{
-                ans += rmax - height[r];
+                result += rmax - height[r];
                 r--;
             }
         }
-
-
-        return ans;
+        return result;
     }
 };
